51a03676561ef5b6db7346d72c1804f09bc46253
ArnavHD <arnavpaul180@gmail.com>
2025-12-06 11:51:16 -0600

    Data show in home page done

--- src/Components/AllLawer/AllLawer.jsx
+++ src/Components/AllLawer/AllLawer.jsx
@@ -6,10 +6,13 @@ const AllLawer = () => {
     const lawers = useLoaderData();
     console.log(lawers);
     return (
-      <div>
+      <div className="grid grid-cols-2 gap-16">
         {lawers.map((lawer) => (
           <Lawer key={lawer.id} lawer={lawer}></Lawer>
         ))}
+        <button className="btn col-span-2 w-52 py-6 px-8 rounded-3xl mx-auto text-[18px] bg-[#0EA106] text-white">
+          Show All Lawers
+        </button>
       </div>
     );
 };
--- src/Components/Lawer/Lawer.jsx
+++ src/Components/Lawer/Lawer.jsx
@@ -6,24 +6,33 @@ const Lawer = ({ lawer }) => {
       lawer;;
   return (
     <div>
-      <div>
-        <div>
-          <img src={image} alt="" />
+      <div className="flex  justify-between items-center gap-16 p-7 border-2 border-black rounded-2xl">
+        <div className="w-1/3">
+          <img
+            className="w-40 h-40 object-cover rounded-xl"
+            src={image}
+            alt=""
+          />
         </div>
-        <div>
-          <div>
-            <p className=" text-[#09982F] bg-[#bef9ce]">{availabilityStatus}</p>
-            <p className=" text-[rgba(23,106,229,1)] bg-[#c4d8f5]">
+        <div className="w-2/3">
+          <div className=" flex gap-2">
+            <p className=" text-[#09982F] bg-[#bef9ce] rounded-xl px-3">
+              {availabilityStatus}
+            </p>
+            <p className=" text-[rgba(23,106,229,1)] bg-[#c4d8f5] rounded-xl px-3">
               {experience}
             </p>
           </div>
           <div>
-            <h1>{name}</h1>
-            <h3>{speciality}</h3>
-            <h4 className='flex'>
+            <h1 className="mt-0.5 text-2xl font-bold">{name}</h1>
+            <h3 className="mt-0.5 text-[18px]">{speciality}</h3>
+            <h4 className="mt-0.5 flex justify-start items-center gap-2">
               <PiTrademarkRegistered />
               License No: {licenseNumber}
             </h4>
+            <button className="btn py-2 mt-4 text-[rgba(23,106,229,1)] w-full rounded-4xl">
+              View Details
+            </button>
           </div>
         </div>
       </div>
